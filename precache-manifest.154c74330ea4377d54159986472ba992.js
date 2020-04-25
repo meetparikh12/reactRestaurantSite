ba6a76c5b812f41b42a6b48272adefe782f3bebd
meetparikh12 <parikhmeet1234@gmail.com>
2020-04-25 13:25:49 +0530

    Updates

---
+++ precache-manifest.154c74330ea4377d54159986472ba992.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ec3c96be2ecc8d4c17a9b04bc3ffb4",
+    "url": "/reactRestaurantSite/index.html"
+  },
+  {
+    "revision": "337b605b40468917ffe6",
+    "url": "/reactRestaurantSite/static/css/main.1fdf49b4.chunk.css"
+  },
+  {
+    "revision": "682717f60fcc6f30d237",
+    "url": "/reactRestaurantSite/static/js/2.305d6d01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactRestaurantSite/static/js/2.305d6d01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "337b605b40468917ffe6",
+    "url": "/reactRestaurantSite/static/js/main.1f738ae0.chunk.js"
+  },
+  {
+    "revision": "a4cfc245cba1377173d3",
+    "url": "/reactRestaurantSite/static/js/runtime-main.d6a21ee7.js"
+  },
+  {
+    "revision": "fcbaeba41efa77a0128ade0af7785569",
+    "url": "/reactRestaurantSite/static/media/foof.fcbaeba4.jpg"
+  },
+  {
+    "revision": "278cde59861a12c2fba1b6559788a029",
+    "url": "/reactRestaurantSite/static/media/img1.278cde59.jpg"
+  },
+  {
+    "revision": "dfad2e0026b143267ceef437a2e30c15",
+    "url": "/reactRestaurantSite/static/media/img_square_1.dfad2e00.jpg"
+  },
+  {
+    "revision": "45ed6f03eb5e89dc705692215077364f",
+    "url": "/reactRestaurantSite/static/media/img_square_2.45ed6f03.jpg"
+  },
+  {
+    "revision": "278cde59861a12c2fba1b6559788a029",
+    "url": "/reactRestaurantSite/static/media/img_square_3.278cde59.jpg"
+  },
+  {
+    "revision": "4b84131d1b1ebe268760e56a84a8182a",
+    "url": "/reactRestaurantSite/static/media/img_square_4.4b84131d.jpg"
+  },
+  {
+    "revision": "018cd4dd32ede2cc3c43572ee64eba44",
+    "url": "/reactRestaurantSite/static/media/img_square_5.018cd4dd.jpg"
+  },
+  {
+    "revision": "e0b7cf383018fe41d4151ec249bbcce5",
+    "url": "/reactRestaurantSite/static/media/landing1.e0b7cf38.png"
+  },
+  {
+    "revision": "0721c80445de3c54df59fdd79303b0aa",
+    "url": "/reactRestaurantSite/static/media/landing2.0721c804.png"
+  },
+  {
+    "revision": "ac51732a2f7878b584845a81d5907c80",
+    "url": "/reactRestaurantSite/static/media/landing3.ac51732a.png"
+  },
+  {
+    "revision": "1d7b217069abe4b68609f7d4b3594a44",
+    "url": "/reactRestaurantSite/static/media/landing4.1d7b2170.png"
+  },
+  {
+    "revision": "a9a75cddb6b73882512f3fa7bae5b91d",
+    "url": "/reactRestaurantSite/static/media/market.a9a75cdd.jpg"
+  }
+]);
