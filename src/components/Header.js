e7293ffb8a3591cdd5acbe7a34f85ebfe6a180fc
meetparikh12 <parikhmeet1234@gmail.com>
2020-04-25 13:50:42 +0530

    Modified components

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
       <header>
         <nav className="navbar navbar-expand-md " id="mainNav">
           <div className="container">
-            <a className="navbar-brand" href="index.html" title="uiCookies:FineOak">
+            <a className="navbar-brand" href="index.html">
               <span
                 className="tit2 t-center"
                 style={{ color: "#fff", fontSize: "34px" }}
@@ -28,32 +28,32 @@ export default class Header extends Component {
             <div className="collapse navbar-collapse" id="navbarResponsive">
               <ul className="navbar-nav ml-auto">
                 <li className="active nav-item">
-                  <a className="nav-link smooth-scroll" href="/">
+                  <a className="nav-link smooth-scroll" href="index.html">
                     WELCOME{" "}
                   </a>
                 </li>
                 <li className="active nav-item">
-                  <a className="nav-link smooth-scroll" href="/">
+                  <a className="nav-link smooth-scroll" href="index.html">
                     ABOUT US
                   </a>
                 </li>
                 <li className="active nav-item">
-                  <a className="nav-link smooth-scroll" href="/">
+                  <a className="nav-link smooth-scroll" href="index.html">
                     SPECIALITIES
                   </a>
                 </li>
                 <li className="active nav-item">
-                  <a className="nav-link smooth-scroll" href="/">
+                  <a className="nav-link smooth-scroll" href="index.html">
                     MENU
                   </a>
                 </li>
                 <li className="active nav-item">
-                  <a className="nav-link" href="/">
+                  <a className="nav-link" href="index.html">
                     RESERVATION TABLE
                   </a>
                 </li>
                 <li className="active nav-item">
-                  <a className="nav-link" href="/">
+                  <a className="nav-link" href="index.html">
                    CONTACT US
                   </a>
                 </li>
